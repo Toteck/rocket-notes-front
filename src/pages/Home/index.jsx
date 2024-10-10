5c5eb0e050164690dd36ad978a56ef87f4f42a30
mateus <mateusweslley14@gmail.com>
2024-10-10 10:02:59 -0300

    menu with filtros

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
   return (
@@ -9,7 +10,17 @@ export function Home() {
         <h1>Rocketnotes</h1>
       </Brand>
       <Header />
-      <Menu></Menu>
+      <Menu>
+        <li>
+          <ButtonText isActive title="Todos" />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Node" />
+        </li>
+      </Menu>
       <Search></Search>
       <Content></Content>
       <NewNote></NewNote>
