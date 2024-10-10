097c42417715d197a3361afe5fc3cf78e09c3d3b
mateus <mateusweslley14@gmail.com>
2024-10-10 16:16:35 -0300

    created note component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiSearch } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
-
+import { Input } from "../../components/Input";
+import { Section } from "../../components/Section";
+import { Note } from "../../components/Note";
 export function Home() {
   return (
     <Container>
@@ -22,8 +24,22 @@ export function Home() {
           <ButtonText title="Node" />
         </li>
       </Menu>
-      <Search></Search>
-      <Content></Content>
+      <Search>
+        <Input icon={FiSearch} placeholder="Pesquisar pelo título" />
+      </Search>
+      <Content>
+        <Section title={"Minhas notas"}>
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: 1, name: "react" },
+                { id: 2, name: "rocketseat" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
       <NewNote>
         <FiPlus />
         Criar nota
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -48,10 +48,13 @@ export const Menu = styled.ul`
 
 export const Search = styled.div`
   grid-area: search;
+  padding: 64px 64px 0;
 `;
 
 export const Content = styled.div`
   grid-area: content;
+  padding: 0 64px;
+  overflow-y: auto;
 `;
 
 export const NewNote = styled.button`
@@ -65,6 +68,6 @@ export const NewNote = styled.button`
   justify-content: center;
 
   svg {
-    margin-right: 8px;
+    margin-right: 0 16px;
   }
 `;
