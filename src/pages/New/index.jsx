3709c64b260beb5f40b04ec22cbf5b4a8cecc412
mateus <mateusweslley14@gmail.com>
2024-10-11 12:02:34 -0300

    finalizando o new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 import { Container, Form } from "./styles";
 
 export function New() {
@@ -22,8 +23,18 @@ export function New() {
 
           <Section title="Links úteis">
             <NoteItem value={"https://rocketseat.com.br"} />
-            <NoteItem isNew />
+            <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title={"Marcadores"}>
+            <div className="tags">
+              <NoteItem value="react" />
+
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
