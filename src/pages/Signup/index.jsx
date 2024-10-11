fe9de1dd7ebea5733daf2b0512d2421884b7b797
mateus <mateusweslley14@gmail.com>
2024-10-10 21:27:22 -0300

    tela signup

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,24 +1,26 @@
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Container, Form, Background } from "./styles";
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
-export function Signin() {
+export function Signup() {
   return (
     <Container>
+      <Background />
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar links úteis</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
 
+        <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="E-mail" type="text" icon={FiMail} />
+
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button title={"Entrar"} />
-        <a href="#">Criar conta</a>
+        <Button title={"Cadastrar"} />
+        <a href="#">Voltar para o login</a>
       </Form>
-      <Background />
     </Container>
   );
 }
