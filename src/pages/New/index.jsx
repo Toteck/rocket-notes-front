24d666f38adf1d98d3813047e2b8436313ebc8fd
mateus <mateusweslley14@gmail.com>
2024-10-11 11:53:58 -0300

    estilizando note item

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
 import { Container, Form } from "./styles";
 
 export function New() {
@@ -17,6 +19,11 @@ export function New() {
 
           <Input placeholder="Título" />
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value={"https://rocketseat.com.br"} />
+            <NoteItem isNew />
+          </Section>
         </Form>
       </main>
     </Container>
